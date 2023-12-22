34af16134f513aa35edb2e5d4fe5c4af14e2b031
holylle <lyaisanshaehova@gmail.com>
2023-12-22 05:17:15 +0300

    Псевдоклассы

---
+++ srcipts/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1144:{
+                items:4
+            }
+        }
+    })
+  });
